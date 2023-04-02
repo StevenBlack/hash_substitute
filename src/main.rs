42519748e58fad6fc36046bef160b2eb1c7a1c83
Steven Black <steveb@stevenblack.com>
2023-04-02 15:54:47 -0400

    Committing latest.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::collections::HashMap;
-use md5;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
 
-fn hash_tokenize(text: &mut str, start: &str, end: &str) -> (String, HashMap<String, String>) {
+fn hash_tokenize(text: &mut String, start: &str, end: &str) -> (String, HashMap<String, String>) {
+    let tokenprefix = "[".repeat(4);
+    let tokensuffix = "]".repeat(4);
     let mut hashes = HashMap::new();
     let mut result = String::new();
     let mut start_index = 0;
-    let mut end_index = 0;
+    let mut end_index;
+    let mut hasher = DefaultHasher::new();
 
     while let Some(start_pos) = text[start_index..].find(start) {
         let start_idx = start_pos + start_index;
@@ -14,18 +18,17 @@ fn hash_tokenize(text: &mut str, start: &str, end: &str) -> (String, HashMap<Str
         if let Some(end_pos) = end_pos {
             end_index = start_idx + start.len() + end_pos;
             let replace_str = &text[start_idx + start.len()..=end_index-1];
-            let hash = format!("{:x}", md5::compute(replace_str));
+            hasher.write(replace_str.as_bytes());
+            let hash = format!("{:x}", hasher.finish());
             hashes.insert(replace_str.to_string(), hash.clone());
             result.push_str(&text[start_index..start_idx]);
-            result.push_str(&format!("[[[{}]]]", hash));
+            result.push_str(&format!("{tokenprefix}{hash}{tokensuffix}"));
             start_index = end_index + end.len();
         } else {
             break;
         }
     }
-
     result.push_str(&text[start_index..]);
-
     (result, hashes)
 }
 
